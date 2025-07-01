c2a8eb819ed55f13333d7ef4846785676c970597
Luca Paulmann <luca.paulmann1@gmail.com>
2025-07-01 18:33:26 +0200

    bump v0.8.4 with centralized versionsystem

--- darktrace/_version.py
+++ darktrace/_version.py
@@ -1,4 +1,4 @@
 # Version information for darktrace-sdk
 # This is the single source of truth for version information
-
-__version__ = "0.8.3"
+# after that the script update_version.py needs to be run
+__version__ = "0.8.4"
